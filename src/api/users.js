f1c5221f197b7635004c73fb3559377bd6fa68e8
Habibou DIEDHIOU <habibodh@gmail.com>
2022-08-21 18:00:30 +0000

    simple user add

--- src/api/users.js
+++ src/api/users.js
@@ -3,6 +3,9 @@ import { api } from './';
 export const getUsers = () =>
     api.get(`/api/v1/users`);
 
+export const addUser = (payload) =>
+    api.post(`/api/v1/users`,payload);
+
 export const modifyUser = (id,payload) =>
     api.put(`/api/v1/users/${id}`,payload);
 
